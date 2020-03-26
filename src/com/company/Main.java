f71997b60a4250f2ba8e14ef34d37ac7c5c6aec9
khmusayev <Pisiklet123!>
2020-03-26 11:51:52 +0100

    some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	     System.out.println("Hello IntelliJ");
+	     System.out.println("Hello IntelliJ + changes");
     }
 }
